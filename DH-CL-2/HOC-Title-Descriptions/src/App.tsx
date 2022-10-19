2e039fe33209df76a4e091919f9f164df31d5218
danieldamian09 <humbertorivero9@gmail.com>
2022-10-19 17:25:26 -0300

    Fix HOC

--- DH-CL-2/HOC-Title-Descriptions/src/App.tsx
+++ DH-CL-2/HOC-Title-Descriptions/src/App.tsx
@@ -2,16 +2,15 @@ import './App.css'
 import { Section } from './components/Section'
 import { WithColorSwap } from './components/WithColorSwap'
 
-function App() {
+const ColorSwapSection = WithColorSwap(Section)
 
+function App() {
 
   // Imagina que tienes tu web personal llena de secciones con títulos y descripciones, y has creado una animación por medio de CSS para que únicamente las secciones que tu quieras cambien de colores 
 
-  // Creamos un nuyevo componene con ColorSwapSection que contiene el HOC Aplicado a Section
-  const ColorSwapSection = WithColorSwap(Section)
-
   return (
     <div className="App">
+      {/* <Section title='Higher Order Component' description='Conociendo los HOC' /> */}
       <ColorSwapSection title='Higher Order Component' description='Conociendo los HOC' />
     </div>
   )
--- DH-CL-2/HOC-Title-Descriptions/src/components/WithColorSwap/WithColorSwap.tsx
+++ DH-CL-2/HOC-Title-Descriptions/src/components/WithColorSwap/WithColorSwap.tsx
@@ -1,15 +1,29 @@
 import { ComponentType } from 'react';
 
 
-export default function WithColorSwap<T extends {}>(Component: ComponentType<T>) {
-	return (props: T) => {
-		return (
-			<div className="color-swap">
-				<Component {...props} />
-			</div>
-		);
-	}
+// export default function WithColorSwap<T extends {}>(Component: ComponentType<T>) {
+// 	return (props: T) => {
+// 		return (
+// 			<div className="color-swap">
+// 				<Component {...props} />
+// 			</div>
+// 		);
+// 	}
 
-};
+// };
+
+export default function WithColorSwap<T extends {}>(WrapperComponent: ComponentType<T>) {
+
+	return function Component(props: T) {
+	
+		return (
+			<div className='color-swap'>
+				<WrapperComponent {...props} />
+			</div>
+
+		)
+	
+	}
+}
 
 
